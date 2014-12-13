a698a8b019efaa3986ed176b0d048a59e6b3e0b3
alexm52 <alexmcdoom@hotmail.com>
2014-12-13 05:01:41 +0000

    Committing decommented .sql source file.

--- cheapo.sql
+++ cheapo.sql
@@ -5,37 +5,37 @@ USE cheapomail;
 */
 
 /*DROP TABLE IF EXISTS user;*/
-/*CREATE TABLE user (
+CREATE TABLE user (
 	id int(5) NOT NULL auto_increment,
 	first_name char(20) NOT NULL default '',
 	last_name char(20) NOT NULL default '',
 	password char(20) NOT NULL,
 	username char(20) NOT NULL,
-	PRIMARY KEY (ID)) ENGINE=MyISAM;*/
+	PRIMARY KEY (ID)) ENGINE=MyISAM;
 
 /*DROP TABLE IF EXISTS message;*/
-/*CREATE TABLE message (
+CREATE TABLE message (
 	id int(10) NOT NULL auto_increment,
 	body text NOT NULL default '',
 	subject char(50) NOT NULL default '',
 	user_id int(5) NOT NULL,
-	/*recipient_ids int(20) NOT NULL,		/
+	/*recipient_ids int(20) NOT NULL,		*/
 	recipient_ids varchar(20) NOT NULL,
-	PRIMARY KEY (ID)) ENGINE=MyISAM;*/
+	PRIMARY KEY (ID)) ENGINE=MyISAM;
 
 /*DROP TABLE IF EXISTS message_read;*/
-/*CREATE TABLE message_read (
+CREATE TABLE message_read (
 	id int(10) NOT NULL auto_increment,
 	message_id char(20) NOT NULL default '',
 	reader_id char(20) NOT NULL default '',
 	date DATETIME NOT NULL,
-	PRIMARY KEY (ID)) ENGINE=MyISAM;*/
+	PRIMARY KEY (ID)) ENGINE=MyISAM;
 
-/*LOCK TABLES user WRITE;
+LOCK TABLES user WRITE;
 INSERT INTO user VALUES (1, 'John', 'Doe', 'bar', 'foo'),
 (2, 'Jane', 'Doe', 'fu', 'sna'), (3, 'Alice', 'Brown', 'wonderland', 'alice'),
 (4, 'Bob', 'Smith', 'sled', 'iambob'), (5, 'Test', 'User', 'pass', 'test');
-UNLOCK TABLES;*/
+UNLOCK TABLES;
 
 LOCK TABLES message WRITE;
 INSERT INTO message VALUES (1, 'Lorem ipsum dolor sit amet...', 'Lorem message', 1, ' 5,'),
